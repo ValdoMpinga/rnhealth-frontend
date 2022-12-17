283eafc1d7ef7ae5b5a8c6333ff91c7a3c10979c
Valdo Mpinga <valdompinga57@gmail.com>
2022-12-17 17:58:58 +0000

    Home page done

---
+++ src/utils/Constants.jsx
@@ -0,0 +1,11 @@
+const RnColors = {
+    rnhealthLightPlum: '#a54a96',
+    rnhealthGrapePurple: '#591f50',
+    rnhealthBlack: '#040404',
+    rnhealthDavyGrey: '#5c545c',
+    rnhealthLiver: '#4c4c4c',
+    rnhealthWhite: '#ffffff',
+    rnhealthSilverChalice: '#ababab'
+};
+
+export default RnColors;
